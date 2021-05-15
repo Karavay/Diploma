4cfcfefcfc8cdef2cdb9738e26b4685d9a394385
Ilya <ilia.karavay@gmail.com>
2021-05-15 10:41:14 +0300

    major update

--- dataHandler/models.py
+++ dataHandler/models.py
@@ -5,12 +5,12 @@ from django.utils import timezone
 
 class UserData(models.Model):
     id = models.CharField('id',max_length = 10,primary_key = True)
-    first_name = models.CharField('first name',max_length = 30)
-    last_name = models.CharField('second name',max_length = 30)
+    first_name = models.CharField('first name',max_length = 100)
+    last_name = models.CharField('last name',max_length = 100)
     sex = models.IntegerField('sex')
     status = models.TextField('user status',null = True)
-    city_id = models.CharField('city_id',max_length = 100,null = True)
-    city_title = models.CharField('city_title',max_length = 100,null = True)
+    city_id = models.CharField('city id',max_length = 100,null = True)
+    city_title = models.CharField('city title',max_length = 100,null = True)
     bdate = models.CharField('birth date',max_length = 15,null = True)
     about = models.TextField('about',null = True)
     activities = models.TextField('activities',null = True)
@@ -18,11 +18,11 @@ class UserData(models.Model):
     career = models.TextField('career',null = True)
     connections = models.TextField('connections',null = True)
     contacts = models.TextField('contacts',null = True)
-    country_id = models.CharField('country_id',max_length = 100,null = True)
-    country_title = models.CharField('country_title',max_length = 100,null = True)
+    country_id = models.CharField('country id',max_length = 100,null = True)
+    country_title = models.CharField('country title',max_length = 100,null = True)
     domain = models.CharField('domain',max_length = 50)
     education = models.TextField('education',null = True)
-    home_town = models.CharField('home town',max_length = 100,null = True)
+    home_town = models.TextField('home town',null = True)
     received_date = models.DateTimeField('time of receiving data',default = timezone.now())
 
     def __str__(self):
--- dataHandler/urls.py
+++ dataHandler/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('',views.mainPage,name='mainPage'),
     path('loadUserData',views.loadUserData,name='loadUserData'),
+    path('visualisation',views.visualisation,name='visualisation'),
+    path('<int:pk>/',views.extendedInfo,name='extendedInfo'),
+    path('visualisationCountries',views.visualisationCountries,name='visualisationCountries'),
+    path('visualisationSex',views.visualisationSex,name='visualisationSex'),
+    path('visualisationAge',views.visualisationAge,name='visualisationAge'),
 ]
